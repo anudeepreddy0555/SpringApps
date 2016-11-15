3e7f01a021fcc0c7c1f1e985f47c71c3237525f7
anudeepreddy0555 <anudeepreddy0555@gmail.com>
2016-11-14 23:03:59 -0700

    creating springMVC project
    created one jar(springapp-lib) module and one war module.
    war(springapp-war) module is dependent on jar module

---
+++ SpringMVCMain/springAppLib/src/main/java/com/springapp/lib/App.java
@@ -0,0 +1,13 @@
+package com.springapp.lib;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
